c8c68850ca6f0743dd323f1d13868dbb7f4652dd
Jun Batingal <batingalnarz11@gmail.com>
2024-09-30 23:21:16 +0800

    port changed

--- localGPTUI/localGPTUI.py
+++ localGPTUI/localGPTUI.py
@@ -61,7 +61,7 @@ def home_page():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--port", type=int, default=5111, help="Port to run the UI on. Defaults to 5111.")
+    parser.add_argument("--port", type=int, default=8111, help="Port to run the UI on. Defaults to 5111.")
     parser.add_argument(
         "--host",
         type=str,
